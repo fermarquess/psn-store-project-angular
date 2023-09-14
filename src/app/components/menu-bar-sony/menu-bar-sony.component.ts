73890714055b4f7c11140c86c8b0d39ce8fae8a9
Fernanda Marques <fermarquessantos@gmail.com>
2023-09-14 17:22:18 -0300

    project created

---
+++ src/app/components/menu-bar-sony/menu-bar-sony.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar-sony',
+  templateUrl: './menu-bar-sony.component.html',
+  styleUrls: ['./menu-bar-sony.component.css']
+})
+export class MenuBarSonyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
